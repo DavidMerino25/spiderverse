61820aac63afd21090426e625d2184ef6466ef3e
David2508 <deividrocan2508@gmail.com>
2022-04-21 23:12:52 -0500

    add class

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,6 +1,15 @@
-describe("Test Suite Dummy Description", () => {
-    test('Case 1 Dummy', () => {
-      const resultOfSomething = 1 + 2
-      expect(resultOfSomething).toBe(18);
+const Spiderman = require('./../app/spiderman')
+describe("Unit test for spiderman Class", () => {
+    test('Case 1 Create an spiderman object', () => {
+      //aquí escribimos el codigo que queremos esxrbir tal cual
+      //Quiero poder instanciar un objeto con esta información
+      const andrewGarfield =new Spiderman("Spiderman Sony",31,"Andrew Garfield",2,"Sony")
+  //Validamos que este codigo funcione
+  expect(andrewGarfield.name).toBe("Spiderman Sony")
+  expect(andrewGarfield.age).toBe(31)
+  expect(andrewGarfield.actor).toBe("Andrew Garfield")
+  expect(andrewGarfield.movies).toBe(2)
+  expect(andrewGarfield.studio).toBe("Sony")
+
     });
   })
